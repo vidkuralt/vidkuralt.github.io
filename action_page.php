5b009313eb1e7b9a21a7bc57bf349efc311d24fe
Vid <47226565+vidkuralt@users.noreply.github.com>
2019-02-03 22:11:50 +0100

    1.1.1.5

--- action_page.php
+++ action_page.php
@@ -1,18 +1,18 @@
 <?php
-$name = $_POST['name'];
-$visiotor_email = ˘$_POST['email'];
-$message = $_POST['message'];
+	$name = $_POST['name'];
+	$visiotor_email = $_POST['email'];
+	$message = $_POST['message'];
 
-$email_from = 'vidkuralt.github.io';
-$email_subject = "New Form Submission";
-$email_body = "User Name:" $name.\n".
-				"User Email: $visiotor_email.\n".
-				    "User Message: $message.\n".
-					
-$to = "kuraltvid@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-mail($to,&email_subject,$email_body,$headers);
-header("Location: contact.html");
+	$email_from = 'vidkuralt.github.io';
+	$email_subject = "New Form Submission";
+	$email_body = "User Name:" $name.\n".
+					"User Email: $visiotor_email.\n".
+						"User Message: $message.\n".
+						
+	$to = "kuraltvid@gmail.com";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
+	mail($to,&email_subject,$email_body,$headers);
+	header("Location: contact.html");
  
 ?>
